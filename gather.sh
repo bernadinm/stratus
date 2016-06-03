b1457a3eca7b1c1b5b96ff48ff3c0a5891a46f42
Miguel Bernadin <miguel.bernadin@gmail.com>
2016-06-03 09:10:10 -0700

    Added bouncer.out

--- gather.sh
+++ gather.sh
@@ -5,6 +5,7 @@ cd mesos-logs-"$(hostname)" && \
 dmesg -T > dmesg.log && \
 journalctl -u dcos-mesos-master -n 1000000 > master-"$(hostname)".log && \
 journalctl -u --no-pager -n 1000000 -u dcos-exhibitor > exhibitor.log && \
+journalctl -u dcos-gunicorn-bouncer.service -n 1000000 --no-pager > bouncer.out && \ 
 cp /opt/mesosphere/active/exhibitor/usr/zookeeper/zookeeper.out . && \
 cd .. && tar -cvf mesos-master-logs-"$(hostname)".tar.gz mesos-logs-"$(hostname)" && \
 rm -fr mesos-logs-"$(hostname)"/
